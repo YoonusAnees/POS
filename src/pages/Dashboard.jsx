a79241922769ba8ee959d219999e9c2847e2e42d
yoonusanees <yoonusaneesniis@gmail.com>
2025-11-26 23:31:07 +0530

    bug fixed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
-import { Card, CardHeader, CardContent } from "../components/ui/Card";
+import { Card, CardHeader, CardContent } from "../components/ui/card";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import {
