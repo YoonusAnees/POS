4dc8618b8ecd6f89e1a6f652bc3e1422c2723771
yoonusanees <yoonusaneesniis@gmail.com>
2025-11-26 23:38:31 +0530

    Deployment

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
-import { Card } from "../components/ui/card";
+import  Card  from "../components/ui/card";
 import { useNavigate } from "react-router-dom";
 
 export default function Sales() {
